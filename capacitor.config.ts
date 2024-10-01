8e4e555ca14f53f82cdf4ca5cb22fa481d6c6a2c
zalmajali <z.almajali@taqnyat.sa>
2024-10-01 21:47:18 +0300

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'سما عبدون',
+  webDir: 'www'
+};
+
+export default config;
